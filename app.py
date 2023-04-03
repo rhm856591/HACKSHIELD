b97ec70fe2985df131de34802a43e9a3798147f6
Rahamtullah Sheikh <sheikh856591@gmail.com>
2023-04-03 17:24:09 +0530

    final

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template 
+from flask import Flask, request, render_template
 import pickle
 
 # Load the model and vectorizer
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
-    
+
 with open('vectorizer.pkl', 'rb') as f:
     vectorizer = pickle.load(f)
 
@@ -14,28 +14,31 @@ app = Flask(__name__)
 # Define the home page route
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', result='')
 
 # Define the route for checking if a message is spam or not
 @app.route('/check', methods=['POST'])
 def check():
+
     # Get the message from the request form
     message = request.form['message']
-    
+
+
     # Vectorize the message
     message_vector = vectorizer.transform([message])
-    
+
     # Predict if the message is spam or not
     is_spam = model.predict(message_vector)[0]
-    
+
     # Display the result
     if is_spam == 1:
-        result = "This message is spam."
+            result = "This messagße is spam."
     else:
-        result = "This message is not spam."
-        
-    return render_template('index.html', id="results", result=result)
+            result = "This message is not spam."
+
+    return render_template('index.html', result=result)
 
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
+
